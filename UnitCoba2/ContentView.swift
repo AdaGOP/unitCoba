f9d4a13fd1f598d260bf8c1431502fd96f0dd971
Haryanto Quang <apple.haryanto@gmail.com>
2021-10-05 13:29:50 +0700

    Initial Commit

---
+++ UnitCoba2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitCoba2
+//
+//  Created by Haryanto Salim on 05/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitCoba2/UnitCoba2App.swift
@@ -0,0 +1,17 @@
+//
+//  UnitCoba2App.swift
+//  UnitCoba2
+//
+//  Created by Haryanto Salim on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitCoba2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
